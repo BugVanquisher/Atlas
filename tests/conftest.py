7fbd9bd837bde1f96eef43c3a6bf4e90e2646718
An Isle <ljygeek@gmail.com>
2025-09-02 22:58:48 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,4 +32,16 @@ def event_loop():
     """Create an instance of the default event loop for the entire session."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
+    pending = asyncio.all_tasks(loop)
+    for task in pending:
+        task.cancel()
+    loop.run_until_complete(loop.shutdown_asyncgens())
     loop.close()
+
+def pytest_sessionfinish(session, exitstatus):
+    try:
+        loop = asyncio.get_event_loop()
+        if hasattr(main, "redis"):
+            loop.run_until_complete(main.redis.close())
+    except Exception:
+        pass
